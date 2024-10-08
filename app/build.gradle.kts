75779947043dfcec61a232093cc57c2f3d96416a
Amro Alfien <55046247+alfienrian@users.noreply.github.com>
2024-10-08 16:18:35 -0700

    uji coba codelab 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,12 +80,11 @@ dependencies {
 
     // Memasukkan dependency untuk material design
     implementation(platform("androidx.compose:compose-bom:2023.06.01"))
-    // other dependencies
-    // Compose
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.compose.material3:material3")
     implementation("com.google.accompanist:accompanist-themeadapter-material3:0.28.0")
-
-
     debugImplementation("androidx.compose.ui:ui-tooling")
+
+    // dependency codelab 2
+    implementation("com.google.android.gms:play-services-ads:22.2.0")
 }
