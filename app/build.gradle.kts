4e427a04ddfb3dc09bf2e3c824e8a5a39b7d12e0
Amro Alfien <55046247+alfienrian@users.noreply.github.com>
2024-10-08 14:54:12 -0700

    feat: codelab `Add Compose to a View-based app`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.10"
     }
 }
 
@@ -77,4 +77,15 @@ dependencies {
     implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
     implementation("com.google.android.material:material:1.11.0")
     ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
+
+    // Memasukkan dependency untuk material design
+    implementation(platform("androidx.compose:compose-bom:2023.06.01"))
+    // other dependencies
+    // Compose
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.compose.material3:material3")
+    implementation("com.google.accompanist:accompanist-themeadapter-material3:0.28.0")
+
+
+    debugImplementation("androidx.compose.ui:ui-tooling")
 }
